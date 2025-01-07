a5b069757f3d4d609d04029129adddde759341aa
EmielD <ej.dorsman@gmail.com>
2025-01-07 19:58:23 +0100

    Added git ignore for binary file, progress on explore command

--- internal/pokecommands/explore.go
+++ internal/pokecommands/explore.go
@@ -1,7 +1,24 @@
 package pokecommands
 
+import (
+	"bootdev/emiel/pokedex/internal/pokeapi"
+	"fmt"
+)
+
 func commandExplore(args ...string) error {
 	location := args[0]
-	println(location)
+	locationDetails, err := pokeapi.GetLocationDetails(location)
+	if err != nil {
+		return fmt.Errorf("error getting the location details: %v", err)
+	}
+
+	if len(locationDetails.PokemonEncounters) == 0 {
+		fmt.Println("No pokemon found in this area...")
+	}
+
+	for _, pokemon := range locationDetails.PokemonEncounters {
+		println(pokemon.Pokemon.Name)
+	}
+
 	return nil
 }
